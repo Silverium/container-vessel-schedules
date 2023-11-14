e020203da2474a87ec0481f42ac699b6ccb1998b
Silverium <soldeplatadeveloper@gmail.com>
2023-11-14 19:28:10 +0100

    Add duration to PortCall and calculate 50th percentile for each port

--- source/app.tsx
+++ source/app.tsx
@@ -15,6 +15,7 @@ type PortCall = {
 	port: Port;
 	arrival: string;
 	departure: string;
+	duration?: number;
 };
 
 type Schedule = {
@@ -42,6 +43,8 @@ export default function App() {
 					if (!portCallsByPort.current[portCall.port.id]) {
 						portCallsByPort.current[portCall.port.id] = [];
 					}
+					// extend the port call with the duration in port
+					portCall.duration = new Date(portCall.departure).getTime() - new Date(portCall.arrival).getTime();
 					portCallsByPort.current[portCall.port.id]!.push(portCall);
 				});
 			});
@@ -49,9 +52,14 @@ export default function App() {
 
 			setAppState("Data processed!");
 			const portCallsByPortArray = Object.entries(portCallsByPort.current).map(([portId, portCalls]) => {
+				// sort the port calls by duration in port
+				portCalls.sort((a, b) => a.duration! - b.duration!);
+				// set the percentile50th for the port
+				const percentile50th = ((portCalls[Math.floor(portCalls.length * 0.5)]?.duration || 0) / 1000 / 60 / 60).toFixed(2);
 				return {
 					portId,
 					numPortCalls: portCalls.length,
+					percentile50th
 				};
 			}).sort((a, b) => b.numPortCalls - a.numPortCalls);
 			setVessels(portCallsByPortArray);
@@ -60,13 +68,55 @@ export default function App() {
 	}, []);
 	return (
 		<Box flexDirection='column'>
-			<Text color="green">
+			<Text color="cyanBright">
 				{appState}
 			</Text>
+			<Text>Total ports: {vessels.length}</Text>
+			<Box gap={5}>
+				<Box flexDirection='column' gap={2} borderStyle="single">
+					<Box borderStyle="doubleSingle">
+						<Text color="green">
+							Top 5 ports with most port calls:
+						</Text>
+					</Box>
 
-			<Text>
-				{JSON.stringify(vessels, null, 2)}
-			</Text>
+					{vessels.slice(0, 5).map((vessel: any, index: number) => {
+						return (
+							<Box flexDirection='row' key={`port-${vessel.portId}-${index}`} gap={1}>
+								<Text color="green">
+									{vessel.portId}:
+								</Text>
+								<Text color="magenta">
+									{vessel.numPortCalls}
+								</Text>
+								<Text color="magenta">
+									50th: {vessel.percentile50th}h
+								</Text>
+							</Box>
+						)
+					})}
+
+				</Box>
+				<Box flexDirection='column' gap={2} borderStyle="single">
+					<Box borderStyle="doubleSingle">
+						<Text color="gray">
+							Bottom 5 ports with fewest port calls:
+						</Text>
+					</Box>
+					{vessels.slice(-5).map((vessel: any, index: number) => {
+						return (
+							<Box flexDirection='row' key={`port-${vessel.portId}-${index}`} gap={1}>
+								<Text color="green">
+									{vessel.portId}:
+								</Text>
+								<Text color="magenta">
+									{vessel.numPortCalls}
+								</Text>
+							</Box>
+						)
+					})}
+				</Box>
+			</Box>
 		</Box>
 	);
 }
